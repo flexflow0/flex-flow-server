831b9605ff992de7b0558eb4c615506b43e41fa1
xtr <arafatrahman862@gmail.com>
2023-08-30 02:21:14 +0600

    jwt, verifyJWT, verifyAdmin added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const SSLCommerzPayment = require('sslcommerz-lts')
 
@@ -7,6 +8,31 @@ const app = express()
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorization access' });
+  }
+// bearer token
+const token = authorization.split(' ')[1];
+
+jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+  if(err){
+    return res.status(401).send({ error: true, message: 'unauthorization access' })
+  }
+  req.decoded = decoded;
+  next();
+})
+
+}
+
+// -----------------------------
+// const decodedEmail = req.decoded.email;
+// if(email !== decodedEmail){
+//   return res.status(403).send({ error: true, message: 'forbidden access' })
+// }
+// --------------------------------
 
 const stripe = require("stripe")(`${process.env.STRIPE_KEY}`);
 
@@ -41,10 +67,47 @@ async function run() {
     const paymentCollection = client.db('flexFlow').collection('payment');
     const SSLPaymentQuery = client.db('flexFlow').collection('SSLPaymentQuery');
 
+
+    // -------- jwt ---------
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.send({ token })
+    })
+// ----------------------------------
+
+// ----------verifyAdmin------------
+const verifyAdmin = async(req, res, next)=>{
+const email = req.decoded.email;
+const query = {email: email}
+const user = await userCollection.findOne(query);
+if(user?.role !== 'admin'){
+  return res.status(403).send({error: true, message: 'forbidden message'})
+}
+next();
+}
+
+
+
+
+app.get('/users/admin/:email', async(req, res)=>{
+  const email = req.params.email;
+
+  if(req.decoded.email !== email){
+      return res.send({ admin: false })
+    }
+  const query = {email: email};
+  const user = await userCollection.findOne(query);
+  const result = {admin: user?.role === 'admin'}
+  res.send(result);
+})
+
     //users
     app.get('/users', async (req, res) => {
-      const cursor = userCollection.find();
-      const result = await cursor.toArray();
+     
+      const result = await userCollection.find().toArray();
       res.send(result)
     })
 
