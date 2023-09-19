ec216100503656e02d7b737af3e2e5fd94118b57
masudra <masudrana6278312@gmail.com>
2023-09-20 02:04:14 +0600

    test

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
@@ -87,18 +86,9 @@ async function run() {
 //  All Users Routes 
     app.get('/users/admin/:email', async (req, res) => {
       const email = req.params.email;
-
-      if (req.decoded.email !== email) {
-        res.send({ admin: false })
-      }
-      const query = { email: email };
+      const query = { email: email, role: "admin" };
       const user = await userCollection.findOne(query);
- 
-      const result = { admin: user?.role === 'admin' }
-      res.send(result);
-     
-
-
+      res.send(user);
     })
 
     //users
